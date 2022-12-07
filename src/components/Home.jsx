bbc1c76c5e23f8f00043bfa0b3169a0bf0eaa231
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-07 09:05:40 +0600

    added about

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Home = () => {
     return (
-      <section className=" flex justify-center my-10 px-5">
-        <div>
+      <section className="container mx-auto lg:px-0 px-5 my-10">
+        <div className='lg:w-3/4'>
           <h4 className="text-lg text-green font-mono">Hi, my name is</h4>
           <h1 className="lg:text-7xl sm:text-6xl text-5xl font-bold text-lightestSlate my-4">
             Khalid Hasan
@@ -11,6 +11,7 @@ const Home = () => {
           <h2 className="lg:text-6xl sm:text-5xl text-4xl text-slate font-bold">
             I build things for the web.
           </h2>
+          <p className='text-lg text-lightSlate my-4'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Laudantium quisquam deserunt maiores ex, quo quae commodi, minus vel praesentium libero nulla nemo magni unde adipisci.</p>
           <button className="text-green border border-green px-4 py-3 mt-5">
             Download Resume
           </button>
