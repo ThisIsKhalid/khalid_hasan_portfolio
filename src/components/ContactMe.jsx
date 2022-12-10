149d3f729476f671c6a9ba9703dce9b59b8c55d2
ThisIsKhalid <iamkhalid.hs@gmail.com>
2022-12-10 17:47:18 +0600

    email send done

--- src/components/ContactMe.jsx
+++ src/components/ContactMe.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import emailjs from "@emailjs/browser";
+import React, { useRef } from "react";
 import { FaEnvelope, FaLocationArrow } from "react-icons/fa";
+import { toast } from "react-toastify";
 
 const ContactMe = () => {
+  const form = useRef();
+
+  const handleSendEmail = (event) => {
+    event.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_50joxvj",
+        "template_nxmv0m5",
+        form.current,
+        "BGeHnHdtbSmPVCABn"
+      )
+      .then(
+        (result) => {
+          toast.success(
+            "Thank you for your message. I will contact with you!!"
+          );
+          event.target.reset();
+        },
+        (error) => {
+          toast.error("something wrong in your email");
+        }
+      );
+  };
+
   return (
     <div id="contact" className="container mx-auto lg:px-0 px-5 py-5 my-24">
       <div className="flex gap-10 mb-10">
@@ -23,24 +50,31 @@ const ContactMe = () => {
           </p>
         </div>
         <div>
-          <form className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 ">
+          <form
+            ref={form}
+            onSubmit={handleSendEmail}
+            className="flex flex-col py-6 space-y-6 md:py-0 md:px-6 "
+          >
             <input
               type="text"
+              name="user_name"
               placeholder="Your Name"
               className="block w-full rounded-md shadow-sm focus:ring focus:outline-none focus:ring-green bg-lightNavy py-2 pl-3 text-white"
             />
             <input
               type="email"
+              name="user_email"
               placeholder="Your Email"
               className="block w-full rounded-md shadow-sm focus:ring focus:outline-none focus:ring-green bg-lightNavy py-2 pl-3 text-white"
             />
             <textarea
               rows="3"
+              name="message"
               placeholder="Message"
               className="block w-full rounded-md shadow-sm focus:ring focus:outline-none focus:ring-green bg-lightNavy py-2 pl-3 text-white"
             ></textarea>
             <button
-              type="button"
+              type="submit"
               className="text-green border border-green hover:bg-lightNavy hover:text-pink hover:border-pink rounded px-4 py-3 flex items-center justify-center gap-2 uppercase"
             >
               Hit me up <FaLocationArrow />
